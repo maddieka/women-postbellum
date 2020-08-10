3f43988b5603b935b7d06d1b0f8f50fd6c4dfd5e
Madison Arnsbarger <maddieka@vt.edu>
2020-08-10 15:24:43 -0400

    fixed, but less convincing

--- 005-correlations.R
+++ 005-correlations.R
@@ -6,13 +6,13 @@ library(Hmisc)
 library(haven)
 
 # load data
-source("git/women-postbellum/01-clean-map-data.R")
+source("~/git/women-postbellum/01-clean-map-data.R")
 votes <- read_dta(file = "~/Documents/Pitt/Projects/women_civil_war/data/ICPSR_08611/DS0001/08611-0001-Data.dta")[,c(1:3,110,115)]
 names(votes) <- c("STATEICP","county_name","COUNTYICP","PresVoteTurnout1860","PctVoteRepublican1860")
 # merge voting data with wctu_data_sf
 wctu_data_sf$churches_per_capita <- wctu_data_sf$churches / wctu_data_sf$totpop
 wctu_data_sf <- merge(x = wctu_data_sf, y = votes, by = c("STATEICP","COUNTYICP"), all.x = TRUE)
-cor_data <- as.data.frame(wctu_data_sf)[,c(25,26,40,41,44:49,53,55:61,64:65,67:69,71:76,87,93,99,84,90,96,102,107:108,110,115,117,121,122,123,133,139,142,143,145:146,120)]
+cor_data <- as.data.frame(wctu_data_sf)[,c("kmw","mainbattlenum","urb860","urb25", "farmval", "homemfg","mfgestab","mfgcap","mfglabm","mfglabf","mfgout","realest","churches","water","rail","fbwtot","Female_lfp_1860","Male_lfp_1860","Female_lfp_1870","Male_lfp_1870","Female_lfp_1880","Male_lfp_1880","Female_lfp_1900","Male_lfp_1900","log_totpop","pct_urb860","pct_pop_died","pct_pop_disabwound","pct_pop_soldiers","pct_pop_mainbattle","pct_pop_whole","pct_pop_wounded","pct_pop_deserted","pct_pop_regout","pop_per_sqmi860","churches_per_capita","PresVoteTurnout1860","PctVoteRepublican1860","pct_pop_disabled")]
 
 # lapply(cor_data, class)
 # res <- cor(cor_data, use = "complete.obs")
@@ -30,5 +30,5 @@ flattenCorrMatrix <- function(cormat, pmat) {
     )
 }
 flattenCorrMatrix(res2$r, res2$P)
-corrplot(res2$r, type="upper", p.mat = res2$P, sig.level = 0.01, insig = "blank", tl.col = "black", tl.cex = 1, na.label = "-")
+corrplot(res2$r, method = "color", type="upper", p.mat = res2$P, sig.level = 0.01, insig = "blank", tl.col = "black", tl.cex = .75, na.label = "-")
 
